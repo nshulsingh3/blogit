882c17bec06c6697583c602c08b77aaebc3918c9
nshulsingh3 <nshulsingh3@gmail.com>
2025-01-11 20:11:38 +0530

    update prisma client version and enable NEXTAUTH_SECRET

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -40,7 +40,7 @@ export const authOptions = {
       return session;
     },
   },
-  // secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.NEXTAUTH_SECRET,
 };
 
 const handler = NextAuth(authOptions);
